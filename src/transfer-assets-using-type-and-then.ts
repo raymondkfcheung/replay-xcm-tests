85fb3d8dfb4dc8e71ee837a6983df43eb477ebde
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-07-14 15:18:31 +0100

    Update samples

--- src/transfer-assets-using-type-and-then.ts
+++ src/transfer-assets-using-type-and-then.ts
@@ -1,10 +1,10 @@
-import { Binary, createClient, Enum } from "polkadot-api";
+import { Binary, createClient, Enum, Transaction } from "polkadot-api";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import { getPolkadotSigner } from "polkadot-api/signer";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import {
-    assetHubWestend,
-    bridgeHubWestend,
+    assetHub,
+    hydration,
     DotXcmVersionedAssetId,
     DotXcmVersionedAssets,
     DotXcmVersionedLocation,
@@ -15,6 +15,8 @@ import {
     XcmV5Junction,
     XcmV5Junctions,
     XcmV5NetworkId,
+    XcmV5WildAsset,
+    XcmV5AssetFilter,
 } from "@polkadot-api/descriptors";
 import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
 import { DEV_PHRASE, entropyToMiniSecret, mnemonicToEntropy, ss58Address } from "@polkadot-labs/hdkd-helpers";
@@ -33,68 +35,59 @@ const toHuman = (_key: any, value: any) => {
     return value;
 };
 
+// https://dev.papi.how/explorer/0xc788a1c9f1a7611210805a90499295d7a2408b0d7108629cad8cac0e3babef39#networkId=polkadot_asset_hub&endpoint=light-client
+// POLKADOT_ASSET_HUB_BLOCK_NUMBER = 9274977
 async function main() {
-    const acalaClient = createClient(
-        withPolkadotSdkCompat(getWsProvider("ws://localhost:8001")),
-    );
     const assetHubClient = createClient(
-        withPolkadotSdkCompat(getWsProvider("ws://localhost:8000")),
+        withPolkadotSdkCompat(getWsProvider("ws://localhost:8000"))
     );
+    const assetHubApi = assetHubClient.getTypedApi(assetHub);
 
-    const assetHubApi = assetHubClient.getTypedApi(assetHubWestend);
-    const bridgeHubApi = acalaClient.getTypedApi(bridgeHubWestend);
+    const hydrationClient = createClient(
+        withPolkadotSdkCompat(getWsProvider("ws://localhost:8001"))
+    );
+    const hydrationApi = hydrationClient.getTypedApi(hydration);
 
     const entropy = mnemonicToEntropy(DEV_PHRASE);
     const miniSecret = entropyToMiniSecret(entropy);
     const derive = sr25519CreateDerive(miniSecret);
     const alice = derive("//Alice");
-    const userPublicKey = alice.publicKey;
-    const aliceSigner = getPolkadotSigner(userPublicKey, "Sr25519", alice.sign);
-    const userAddress = ss58Address(userPublicKey);
+    const alicePublicKey = alice.publicKey;
+    const aliceSigner = getPolkadotSigner(alicePublicKey, "Sr25519", alice.sign);
+    const aliceAddress = ss58Address(alicePublicKey);
 
-    const origin = Enum("system", Enum("Signed", userAddress))
-
+    const origin = Enum("system", Enum("Signed", aliceAddress))
     const dest = DotXcmVersionedLocation.V4({
         parents: 1,
         interior: XcmV5Junctions.X1(
-            XcmV5Junction.Parachain(2313)
+            XcmV5Junction.Parachain(2034)
         )
     });
-
-    const reserve = Enum("RemoteReserve", DotXcmVersionedLocation.V4({
-        parents: 1,
-        interior: XcmV5Junctions.X1(XcmV5Junction.Parachain(1000)),
-    }));
-
+    const reserve = Enum("LocalReserve");
     const assetId = {
-        parents: 1,
-        interior: XcmV5Junctions.X2([
-            XcmV5Junction.GlobalConsensus(XcmV5NetworkId.ByGenesis(Binary.fromHex("0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e"))),
-            XcmV5Junction.Parachain(2313),
-        ]),
+        parents: 2,
+        interior: XcmV5Junctions.X1(
+            XcmV5Junction.GlobalConsensus(XcmV5NetworkId.Kusama()),
+        ),
     };
-
     const customXcmOnDest = DotXcmVersionedXcm.V4([
         XcmV5Instruction.DepositAsset({
-            assets: {
-                type: "Wild",
-                value: { type: "AllCounted", value: 1 }
-            },
+            assets: XcmV5AssetFilter.Wild(XcmV5WildAsset.All()),
             beneficiary: {
-                parents: 1,
+                parents: ,
                 interior: XcmV5Junctions.X1(
-                    XcmV5Junction.AccountKey20({ key: Binary.fromHex("0x302f0b71b8ad3cf6dd90adb668e49b2168d652fd") })
+                    XcmV5Junction.AccountId32({ id: Binary.fromHex("0xa2c0ccb3b953bb79a4109ce8f56d1538bc5ac2d8f428f4531df973bb7e3c7c13") })
                 )
             }
         })
     ]);
 
-    const tx: any = assetHubApi.tx.PolkadotXcm.transfer_assets_using_type_and_then({
+    const tx: Transaction<any, string, string, any> = assetHubApi.tx.PolkadotXcm.transfer_assets_using_type_and_then({
         dest,
         assets: DotXcmVersionedAssets.V4([
             {
                 id: assetId,
-                fun: XcmV3MultiassetFungibility.Fungible(100_000_000n),
+                fun: XcmV3MultiassetFungibility.Fungible(3_823_214_300_643n),
             }
         ]),
         assets_transfer_type: reserve,
@@ -103,18 +96,83 @@ async function main() {
         custom_xcm_on_dest: customXcmOnDest,
         weight_limit: XcmV3WeightLimit.Unlimited(),
     });
-    const decodedCall = tx.decodedCall;
+    const decodedCall = tx.decodedCall as any;
     console.log("👀 Executing XCM:", JSON.stringify(decodedCall, toHuman, 2));
 
-    const dryRunResult: any = await bridgeHubApi.apis.DryRunApi.dry_run_call(
+    const dryRunResult: any = await assetHubApi.apis.DryRunApi.dry_run_call(
         origin,
         decodedCall,
         XCM_VERSION,
     );
     console.log("📦 Dry run result:", JSON.stringify(dryRunResult.value, toHuman, 2));
 
-    acalaClient.destroy();
+    const {
+        execution_result: executionResult,
+        emitted_events: _emmittedEvents,
+        local_xcm: _localXcm,
+        forwarded_xcms: _forwardedXcms,
+    } = dryRunResult.value;
+
+    if (!dryRunResult.success || !executionResult.success) {
+        console.error("❌ Local dry run failed!");
+    } else {
+        console.log("✅ Local dry run successful.");
+
+        const hydrationBlockBefore = await hydrationClient.getFinalizedBlock()
+
+        const ev = await tx.signAndSubmit(aliceSigner);
+        console.log(`📦 Finalised on Polkadot Asset Hub in block #${ev.block.number}: ${ev.block.hash}`);
+
+        if (!ev.ok) {
+            const dispatchError = ev.dispatchError;
+            if (dispatchError.type === "Module") {
+                const modErr: any = dispatchError.value;
+                console.error(`❌ Dispatch error in module: ${modErr.type} → ${modErr.value?.type}`);
+            } else {
+                console.error("❌ Dispatch error:", JSON.stringify(dispatchError, toHuman, 2));
+            }
+        }
+
+        const sentEvents = await assetHubApi.event.PolkadotXcm.Sent.pull();
+        if (sentEvents.length > 0) {
+            const sentMessageId = sentEvents[0].payload.message_id.asHex();
+            console.log(`📣 Last message Sent on Polkadot Asset Hub: ${sentMessageId}`);
+
+            let processedMessageId = undefined
+            const maxRetries = 8;
+            for (let i = 0; i < maxRetries; i++) {
+                const hydrationBlockAfter = await hydrationClient.getFinalizedBlock()
+                if (hydrationBlockAfter.number == hydrationBlockBefore.number) {
+                    const waiting = 1_000 * (i + 1);
+                    console.log(`⏳ Waiting ${waiting}ms for Hydration block to be finalised (${i + 1}/${maxRetries})...`);
+                    await new Promise(resolve => setTimeout(resolve, waiting));
+                    continue;
+                }
+
+                console.log(`📦 Finalised on Hydration in block #${hydrationBlockAfter.number}: ${hydrationBlockAfter.hash}`);
+                const processedEvents = await hydrationApi.event.MessageQueue.Processed.pull();
+                if (processedEvents.length > 0) {
+                    processedMessageId = processedEvents[0].payload.id.asHex();
+                    console.log(`📣 Last message Processed on Hydration: ${processedMessageId}`);
+                } else {
+                    console.log("📣 No Processed events on Hydration found.");
+                }
+
+                break;
+            }
+
+            if (processedMessageId === sentMessageId) {
+                console.log("✅ Message ID matched.");
+            } else {
+                console.error("❌ Processed message ID does not match sent message ID.");
+            }
+        } else {
+            console.log("📣 No Sent events on Polkadot Asset Hub found.");
+        }
+    }
+
     assetHubClient.destroy();
+    hydrationClient.destroy();
 }
 
 main().catch(console.error);
